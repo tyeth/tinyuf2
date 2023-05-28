df09c98b81751825c347070315a74209b8ad9bbb
Tyeth Gundry <tyethgundry@googlemail.com>
2023-05-28 18:39:17 +0100

    define T-QT

---
+++ ports/espressif/boards/lilygo_ttgo_t-qt/board.h
@@ -0,0 +1,62 @@
+/* 
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Ha Thach (tinyusb.org) for Adafruit Industries
+ * Copyright (c) 2022 Linar Yusupov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#define BOARD_INIT_CUSTOM 1
+
+//--------------------------------------------------------------------+
+// Button
+//--------------------------------------------------------------------+
+
+// Enter UF2 mode if GPIO is pressed while 2nd stage bootloader indicator
+// is on e.g RGB = Purple. If it is GPIO0, user should not hold this while
+// reset since that will instead run the 1st stage ROM bootloader
+#define PIN_BUTTON_UF2    0
+
+// Initial delay in milliseconds to detect user interaction to enter UF2
+#define UF2_DETECTION_DELAY_MS  1000
+
+//--------------------------------------------------------------------+
+// LED
+//--------------------------------------------------------------------+
+
+// LED for indicator and writing flash
+#define LED_PIN           6
+#define LED_STATE_ON      1
+
+//--------------------------------------------------------------------+
+// USB UF2
+//--------------------------------------------------------------------+
+
+#define USB_VID           0x303A            // Espressif VID
+#define USB_PID           0x8155            // Espressif assigned PID
+#define USB_MANUFACTURER  "LilyGO"
+#define USB_PRODUCT       "T-QT Pro"
+
+#define UF2_PRODUCT_NAME  USB_MANUFACTURER " " USB_PRODUCT
+#define UF2_BOARD_ID      "ESP32S3-T-QT Pro"
+#define UF2_VOLUME_LABEL  "TQTBOOT"
+#define UF2_INDEX_URL     "https://www.lilygo.cc/en-cn/products/t-qt-pro"
